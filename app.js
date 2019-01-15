cb5036cf521f2e0ce891d6bbdef7e8b49a310429
Maher <maher91syr@gmail.com>
2019-01-15 23:27:15 +0900

    JS change

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('.score-board');
 const result_div = document.querySelector('.result');
 const rock_div = document.getElementById('r');
-const paper_div = document.getElementById('p');
-const scissors_div = document.getElementById('s');
+//const paper_div = document.getElementById('p');
+//const scissors_div = document.getElementById('s');
