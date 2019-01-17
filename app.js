1c013c5f967bb1c5b7b6333e41f2d1f69d0688fb
Maher <maher91syr@gmail.com>
2019-01-17 23:52:31 +0900

    JS

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('.score-board');
 const result_div = document.querySelector('.result');
-const rock_div = document.getElementById('r');
+//const rock_div = document.getElementById('r');
 //const paper_div = document.getElementById('p');
 //const scissors_div = document.getElementById('s');
