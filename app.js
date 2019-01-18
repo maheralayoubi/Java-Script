a5a5dc721d19b79cfdfc5289410ad54ab307c6b5
Maher <maher91syr@gmail.com>
2019-01-18 12:43:07 +0900

    Adding the functions

--- app.js
+++ app.js
@@ -4,6 +4,34 @@ const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('.score-board');
 const result_div = document.querySelector('.result');
-//const rock_div = document.getElementById('r');
-//const paper_div = document.getElementById('p');
-//const scissors_div = document.getElementById('s');
+const rock_div = document.getElementById('r');
+const paper_div = document.getElementById('p');
+const scissors_div = document.getElementById('s');
+
+function getComputerChoice() {
+  const choices = ['r', 'p', 's'];
+  // Math.random gets random numbers brtween 0 and 3
+  // Math.floor  gets rid of the decimal number. So, the choices will be (0,1,2)
+  console.log(Math.floor(Math.random() * 3);
+}
+getComputerChoice();
+
+function game(userChoice) {
+  console.log('User choice is: ' + userChoice)
+}
+
+function main(){
+  rock_div.addEventListener('click', function() {
+    game('r');
+  })
+
+  paper_div.addEventListener('click', function() {
+    game('p');
+  })
+
+  scissors_div.addEventListener('click', function() {
+    game('s');
+  })
+}
+
+main();
