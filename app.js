f42abf7434b78387ee5390f71044243b34e37c6e
Maher <maher91syr@gmail.com>
2019-02-12 22:43:50 +0900

    Making the functions

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('.score-board');
@@ -17,7 +17,10 @@ function getComputerChoice() {
 }
 
 function win() {
-  console.log("WIN");
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  console.log(userScore);
 }
 
 function lose() {
@@ -49,7 +52,6 @@ function game(userChoice) {
     break;
   }
 }
-
 function main(){
   rock_div.addEventListener('click', function() {
     game('r');
